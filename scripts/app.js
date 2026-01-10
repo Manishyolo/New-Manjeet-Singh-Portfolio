a1cec46825e033003c4e3602a98df39a58ff92e7
Manish kumar <manish04kumarsingh@gmail.com>
2026-01-10 21:14:21 +0530

    created functions to create an array of url of different type of media like video and image for reusabilty purpose

--- scripts/app.js
+++ scripts/app.js
@@ -1,18 +1,40 @@
-// Initialize Lenis
-const lenis = new Lenis({
-  autoRaf: true,
-});
+const Animations = MakeArrayofMedia(
+  document.querySelectorAll(".animation-container")
+);
 
-// Listen for the scroll event and log the event data
-lenis.on('scroll', (e) => {
- 
-});
+const Reels = MakeArrayofMedia(document.querySelectorAll(".reel-container"));
 
-   var swiper = new Swiper(".gallery-container", {
-        slidesPerView: 1,
-        spaceBetween: 30,
-        pagination: {
-          el: ".swiper-pagination",
-          clickable: true,
-        },
-      });
+const Illustrations = MakeArrayofMedia(
+  document.querySelectorAll(".illustration-container")
+);
+
+function MakeArrayofMedia(data) {
+  let MediaArray;
+  const url = data[0].children[0].attributes.src.value;
+  const type = getMediaType(url);
+
+  if (!data) {
+    return console.error("Please provide a html collection");
+  }
+  if (type === "video") {
+    MediaArray = [...data].map((elem) => {
+      return elem.children[0].attributes.src.value;
+    });
+  }
+  if (type === "image") {
+    MediaArray = [...data].map((elem) => {
+      return elem.children[0].attributes.src.value;
+    });
+  }
+  return MediaArray;
+}
+
+function getMediaType(url) {
+  const imageExt = ["jpg", "jpeg", "png", "gif", "webp", "svg"];
+  const videoExt = ["mp4", "webm", "ogg", "mov", "avi"];
+  const ext = url.split(".").pop().toLowerCase();
+  if (imageExt.includes(ext)) return "image";
+  if (videoExt.includes(ext)) return "video";
+
+  return "unknown";
+}
---
+++ scripts/library.js
@@ -0,0 +1,18 @@
+// Initialize Lenis
+const lenis = new Lenis({
+  autoRaf: true,
+});
+
+// Listen for the scroll event and log the event data
+lenis.on('scroll', (e) => {
+ 
+});
+
+   var swiper = new Swiper(".gallery-container", {
+        slidesPerView: 1,
+        spaceBetween: 30,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+      });
